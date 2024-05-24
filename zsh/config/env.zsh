7691ed26dbffe2d81cb72310e4a625080b66b441
qomaeng <qomaeng@gmail.com>
2024-05-25 04:47:24 +0900

    wip

--- zsh/config/env.zsh
+++ zsh/config/env.zsh
@@ -5,7 +5,7 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-export PATH="$HOME/.yarn/bin:$PATH"
+# export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
@@ -68,5 +68,8 @@ export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_CTRL_T_OPTS=$FZF_DEFAULT_OPTS
 export FZF_ALT_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_ALT_T_OPTS=$FZF_DEFAULT_OPTS
+
+## Rust
+export RUSTC_WRAPPER=`which sccache`
 
 # vim:ft=zsh:nowrap
