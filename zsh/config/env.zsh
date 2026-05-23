b102323bcec9124e17e39f5c6cb73b845aae6fc5
qomaeng <qomaeng@gmail.com>
2026-05-23 15:29:54 +0900

    change language es_US -> C

--- zsh/config/env.zsh
+++ zsh/config/env.zsh
@@ -1,13 +1,11 @@
-export LANGUAGE=en_US:en
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LC_MESSAGES=en_US.UTF-8
+export LANG="C.utf8"
+export LC_ALL="C.utf8"
 
 export TERMINAL="alacritty"
 export BROWSER="firefox"
 export VISUAL="nvim"
 export EDITOR="nvim"
 
-export HISTFILE=$HOME/.zsh_history
+export HISTFILE="$HOME/.zsh_history"
 
 export PATH="$HOME/.local/bin:$PATH"
