7aeef658b3b3602f893b91c744fdd07bf51acfa4
qomaeng <qomaeng@gmail.com>
2026-05-23 15:29:24 +0900

    remove useless fzf options

--- zsh/config/plugins/fzf.zsh
+++ zsh/config/plugins/fzf.zsh
@@ -1,6 +1,4 @@
-export FZF_DEFAULT_COMMAND='rg --files --hidden --column --line-number --no-heading --smart-case --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden -glob "!.git/*"'
 export FZF_DEFAULT_OPTS='--reverse --cycle --height=40%'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_CTRL_T_OPTS=$FZF_DEFAULT_OPTS
-export FZF_ALT_T_COMMAND=$FZF_DEFAULT_COMMAND
-export FZF_ALT_T_OPTS=$FZF_DEFAULT_OPTS
